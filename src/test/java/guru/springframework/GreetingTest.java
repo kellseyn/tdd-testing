e24a6164eb2a17429c3fd73785a59a2cb093d4a2
Kellsey Neagley <kellseyneagley@gmail.com>
2020-07-24 15:37:09 -0500

    went thru the whole maven junit testing setup

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -1,22 +1,51 @@
 package guru.springframework;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GreetingTest {
 
+    private Greeting greeting;
+
+    @BeforeAll
+    public static void  beforeClass(){
+        System.out.println("Before - I am only called once!");
+    }
+
+    @BeforeEach
+    void setUp(){
+        System.out.println("In Before Each...");
+        greeting = new Greeting();
+
+    }
+
     @Test
     void helloWorld() {
-        Greeting greeting = new Greeting();
 
         System.out.println(greeting.helloWorld());
     }
 
     @Test
-    void testHelloWorld() {
-        Greeting greeting = new Greeting();
+    void helloWorld1() {
 
         System.out.println(greeting.helloWorld("Kellsey"));
     }
+
+    @Test
+    void helloWorld2() {
+
+        System.out.println(greeting.helloWorld("Zackry"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("In After Each...");
+    }
+
+    @AfterAll
+    public static void  afterClass(){
+        System.out.println("After - I am only called once!");
+    }
+
 }
